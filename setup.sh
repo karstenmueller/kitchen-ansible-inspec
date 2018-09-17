7051868dd47ec0c43000f1906fa5d6de57ea990b
Karsten Müller <info@kamueller.de>
2018-09-17 22:25:04 +0200

    moved setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,43 @@ cmd() {
     echo "$cmd not installed" >&2
     return 1
   fi
-  echo "$cmd is installed" >&2
+  echo "$cmd already installed" >&2
 }
 
-cmd brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-cmd vagrant || brew cask install vagrant
-cmd vagrant || brew cask install virtualbox
+setup_linux() {
+  # bundler
+  cmd bundler || sudo gem install bundler
+  # inspec
+  cmd inspec || curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+  # ansible
+  cmd ansible || (
+    sudo apt-get update;
+    sudo apt-get -y install software-properties-common;
+    sudo apt-add-repository ppa:ansible/ansible;
+    sudo apt-get update;
+    sudo apt-get -y install ansible
+  )
+  # virtualbox, vagrant
+  cmd virtualbox || sudo apt-get -y virtualbox
+  cmd vagrant || sudo apt-get -y vagrant
+}
 
-bundle install
+setup_macos() {
+  # bundler
+  cmd bundler || sudo gem install bundler
+  # package manager
+  cmd brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # virtualbox, vagrant
+  cmd vagrant || brew cask install vagrant
+  cmd virtualbox || brew cask install virtualbox
+}
+
+ostype() {
+  case $OSTYPE in
+    linux*) setup_linux;;
+    darwin*) setup_macos;;
+  esac
+}
+
+# bundler using Gemfile
+bundle install --retry=3 --jobs=4
